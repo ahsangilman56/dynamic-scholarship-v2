61cb8e84ab6799a60fa8655b8924e0b287fe1b4e
Ahsan Gilman <ahsan.gilman@gmail.com>
2026-04-12 01:59:03 +0600

    Initial deploy of dynamic portal replica

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'scholarship-local-v3';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-512.png',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Stale-while-revalidate for assets, but always fetch GitHub data fresh in index.html
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  
+  // Ensure data.json is always fetched fresh
+  if (event.request.url.includes('data.json')) {
+    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      const fetchPromise = fetch(event.request).then(networkResponse => {
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, networkResponse.clone()));
+        return networkResponse;
+      }).catch(() => cachedResponse);
+      
+      return cachedResponse || fetchPromise;
+    })
+  );
+});
